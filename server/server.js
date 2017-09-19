adf6f6b96a273b0f7c0a43a07751701baa7aa2fc
Sergio Rueda <sergio4234@msn.com>
2017-09-19 12:02:01 -0500

    Add geolocation support via api

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-
+const {generateLocationMessage} = require('./utils/message');
 const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -30,13 +30,11 @@ io.on('connection', (socket) => {
       console.log('From client: createMessage',message);
       io.emit('newMessage', generateMessage(message.from, message.text));
       callback('This is from server');
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
-
    });
+
+   socket.on('createLocationMessage', (coords) => {
+     io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude ))
+   })
 
     socket.on('disconnect', () => {
       console.log('client has disconnected');
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,7 @@
 var expect = require('expect');
 
 var {generateMessage} = require('./message');
+var {generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
    it('Should generate correct message object', () => {
@@ -13,8 +14,21 @@ describe('generateMessage', () => {
        from: from,
        text: text
      });
+   });
+});
 
-   })
 
+describe('generateLocationMessage', () => {
+   it('Should generate correct Location message object', () => {
+     var from = 'serge';
+     var latitude  = 41.9943555;
+     var longitude = -87.7565034;
+     var url = 'https://www.google.com/maps?q=41.9943555,-87.7565034';
 
-})
+     var message = generateLocationMessage(from, latitude, longitude);
+
+     expect(message.createdAt).toBeA('number');
+     expect(message.url).toEqual(url);
+     expect(message).toInclude({from});
+});
+});
